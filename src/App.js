35ca32635e1b07ef4f8e79c539dfa03323dbe99c
Laura Kapitza <laura.kapitza@gmail.com>
2021-06-24 21:38:34 +0200

    iteration 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import Contacts from './Contacts.js';
 import './App.css';
 
 class App extends React.Component {
+  
   render() {
+    
     return (
       <div className="App" >
+        <h1>IronContacts</h1>
+        <table>
+          <thead>
+              <tr>
+                  <th>Picture</th>
+                  <th>Name</th>
+                  <th>Popularity</th>
+              </tr>
+          </thead>
+          <Contacts />
+        </table>
       </div>
     );
   }
